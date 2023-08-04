dda1ee9f345ba02582c7e11eebececc5c668bfb3
antoniocap2023 <138351921+antoniocap2023@users.noreply.github.com>
2023-08-04 11:44:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function maxClick(){
+   img1 = document.getElementById("img1");
+   img1.style.visibility="visible";
+   document.getElementById("img2").style.visibility = "visible";  
+}
+function hideMaxes(){
+    document.getElementById("img1").style.visibility = "hidden"; 
+   document.getElementById("img2").style.visibility = "hidden"; 
+}
